bed502a7f4013e5d97b1ccc31b2b1a82e79e69a3
Prink30 <priyankabuvana3@gmail.com>
2022-08-26 16:18:16 +0530

    fc

---
+++ stringsExample/src/Basics/string2.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class string2 {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringBuilder s = new StringBuilder("Reena");
+		
+		s.append("number");
+		System.out.println(s );
+		System.out.println("sanu");
+		
+		
+	}
+
+}
